5b7d5d2fc130c4a3a2fa5b3cda67aeb78c2018af
andresjaromezuk <andres.jaromezuk@gmail.com>
2024-07-09 20:15:20 -0300

    adding mlflow registry

--- eval_classifier.py
+++ eval_classifier.py
@@ -4,13 +4,22 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
 
-# %%
+mlflow.set_experiment("Bayesian classifier")
+
 df_train = pd.read_csv("./data/alturas-pesos-mils-train.csv")
 df_test  = pd.read_csv("./data/alturas-pesos-mils-test.csv")
 # %%
-#model = GaussianNB()
-#model = QuadraticDiscriminantAnalysis(store_covariance=True)
-model = LinearDiscriminantAnalysis(store_covariance=True)
+model_list = [
+  {'model': GaussianNB, 'model_name': 'GaussianNB'},
+  {'model': QuadraticDiscriminantAnalysis, 'model_name': 'QuadraticDiscriminantAnalysis'},
+  {'model': LinearDiscriminantAnalysis, 'model_name': 'LinearDiscriminantAnalysis'},
+]
+model_selected = model_list[1]
+model = model_selected['model'](reg_param=0.8)
+mlflow.start_run(run_name=model_selected['model_name'])
+
+mlflow.log_params({'reg_param': 0.8})
+
 
 # %%
 #entrenamiento modelo
@@ -23,7 +32,7 @@ output_test = df_test[['Genero']]
 model.fit(input_train, output_train)
 
 # %%
-model.classes_
+#model.classes_
 
 #NaiveBayes
 #model.class_count_ #conteo
@@ -31,8 +40,8 @@ model.classes_
 #model.var_ #varianzas
 
 #QDA
-model.means_
-model.covariance_
+#model.means_
+#model.covariance_
 
 # %%
 #predicción
@@ -43,11 +52,12 @@ model.covariance_
 
 #accuracy
 train_accuracy = model.score(input_train, output_train)
-train_accuracy
+test_accuracy = model.score(input_test, output_test)
+
+mlflow.log_metrics({'train_accuracy': train_accuracy,'test_accuracy':test_accuracy})
+
+mlflow.end_run()
+
+
 
 # %%
-test_accuracy = model.score(input_test, output_test)
-test_accuracy
-
-
-
